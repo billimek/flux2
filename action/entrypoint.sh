011e916495572af7978c108fbefec31c200113cd
Jeff Billimek <jeff@billimek.com>
2020-12-01 17:09:20 -0500

    debugging path setting
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -31,7 +31,11 @@ curl -sL $BIN_URL | tar xz -C "$HOME/bin/"
 # Make binary executable
 chmod +x "$HOME/bin/flux"
 
+echo "(pre) PATH is: $PATH"
+
 # Add binary to GitHub runner path
 echo "$HOME/bin" >> $GITHUB_PATH
 
-flux -v
+echo "(post) PATH is: $PATH"
+
+"$HOME/bin/flux" -v
